30d6ddce6e1e8e569a55a0b4f415c0f00103c8d3
Asutosh <asutoshsahoo786@gmail.com>
2016-10-10 01:15:59 +0530

    error handled

--- myCSVParser.cpp
+++ myCSVParser.cpp
@@ -1,7 +1,7 @@
 #define READ 1
 #define WRITE 2
 #define APPEND 4
-
+#define ERROR 
 class CSVParser{
 	string filehandle;
 	int records;
@@ -17,37 +17,35 @@ public:
 		/*everytime check if the mode allows the function to be executed or not
 i.e, if user wants to add or delete record, check if mode is write or append, otherwise return false
 */
-		int countRecords()
-		{
-			int noOfRecords = 0;
-			ifstream f1(filehandle,ios::in);
-			while(!f1.eof())
-			{
-				if(f1.get() == '\n')
-					noOfRecords++;
-			}
-			f1.close();
-			return noOfRecords;
-		}
-		int countAttributes()
-		{
-			ifstream f1(filehandle,ios::in);
-			int noOfAttributes = 0;
-			char c;
-			do{
-				c = f1.get();
-				if(c == ',')
-					noOfAttributes++;
-			}while(c != '\n');
-			noOfAttributes++;
-			return noOfAttributes;
-		}
-		string** getRecords()
-		{
-			
-
-
-			f1.close();
-		}
 	}
-}
+
+	int countRecords()
+	{
+		int noOfRecords = 0;
+		ifstream f1(filehandle,ios::in);
+		while(!f1.eof())
+		{
+			if(f1.get() == '\n')
+				noOfRecords++;
+		}
+		f1.close();
+		return noOfRecords;
+	}
+	int countAttributes()
+	{
+		ifstream f1(filehandle,ios::in);
+		int noOfAttributes = 0;
+		char c;
+		do{
+			c = f1.get();
+			if(c == ',')
+				noOfAttributes++;
+		}while(c != '\n');
+		noOfAttributes++;
+		return noOfAttributes;
+	}
+
+};
+
+
+
